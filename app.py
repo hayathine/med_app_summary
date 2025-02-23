3414576942aeb83f3b6c76a5d69f9839ec0a24aa
hayathine <97364703+hayathine@users.noreply.github.com>
2025-02-23 15:46:17 +0900

    マトリックス図の修正

--- app.py
+++ app.py
@@ -1,53 +1,23 @@
 #%%
 import json
+import yaml
 import pandas as pd
 import plotly.graph_objects as go
+from map_layout import MapLayout
 
-# jsonファイルの読み込み
-# app_list.jsonファイルを読み込む
+layout = MapLayout()
+
+# サービスファイルの読み込み
 with open('app_list.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
-
 df = pd.DataFrame(data["services"])
 
-# layoutの設定
-layout = go.Layout(
-    title="med_app_mappping_matrix",
-    xaxis=dict(title="ターゲット",
-            range=[0, 20]),
-    yaxis=dict(title="アプローチ",
-            range=[0, 20]),
-    hovermode="closest"
-)
-
 # mapping_matrixの作成
-fig = go.Figure(layout=layout)
+fig = go.Figure(layout=layout.set_layout())
+fig.add_trace(layout.set_scatter(df))
 
-
-#%%
-# カテゴリと色の対応を定義
-color_map = {
-    "健康管理": "blue",
-    "業務効率化": "green",
-    "問診": "red",
-    "診断補助": "purple",
-    "コミュニケーション": "orange"
-}
-
-fig.add_trace(go.Scatter(
-    x=df["X"],
-    y=df["Y"],
-    mode="markers+text",
-    text=df["name"],
-    textposition="top center",
-    marker=dict(
-        size=20,
-        color=df["category"].map(color_map)
-    ),
-    customdata=df["url"],
-    hovertemplate="<b>%{text}</b><br>Click to open URL<extra></extra>"
-))
+fig.show()
 #%% 
 # PNGファイルを作成
-fig.write_image("images/med_app_scatter.png",engine='orca')
+# fig.write_image("images/med_app_scatter.png",engine='orca')
 
