4d2ea100b77dce855d9e54e725d293e71f9934a7
hayathine <97364703+hayathine@users.noreply.github.com>
2025-02-25 15:07:00 +0900

    update create-new-data

--- create-new-data.js
+++ create-new-data.js
@@ -6,9 +6,8 @@ console.log("issueJson:", process.argv[2]);
 console.log('typeof ' + (typeof issueJson));
 const body = process.argv[2];
 const id = process.argv[2];
-console.log("id:", id);
-console(process.argv)
+
 const fileName = `add-new-data-${id}.txt`;
 const filePath = path.join('assets', fileName); // assetsフォルダへのパス
-
+console(process.argv);
 fs.writeFileSync(filePath, body);
