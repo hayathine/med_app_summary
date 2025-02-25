a740a30d02303f91f0a3db8335cc25d99332f7d1
hayathine <97364703+hayathine@users.noreply.github.com>
2025-02-25 15:48:16 +0900

    update

--- create-new-data.js
+++ create-new-data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const urlPattern = /^https?:\/\//;
+const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(:[0-9]{1,5})?(\/.*)?$/;
 const body = process.argv[2].split('\n');
 console.log(body);
 const id = process.argv[3];
@@ -13,12 +13,15 @@ if (name.length > 100) {
     console.error("Error: name must be 100 characters or less.");
     process.exit(1);
 }
+const arr = [name, url];
+console.log(arr);
 // url のバリデーション
-if (!urlPattern.test(url) || url.length > 200) {
-    console.error("Error: url must be a valid http(s) URL and 200 characters or less.");
+if (!urlPattern.test(url) || url.length > 300) {
+    console.error("Error: url must be a valid http(s) URL and 300 characters or less.");
     process.exit(1);
 }
+
 const fileName = `add-new-data-${id}.txt`;
 const filePath = path.join('assets', fileName); // assetsフォルダへのパス
 const str = JSON.stringify(arr);
-fs.writeFileSync(filePath, bstrody);
+fs.writeFileSync(filePath, str);
