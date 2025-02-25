b3b388119df496ab49b503986d071ae34a77518a
hayathine <97364703+hayathine@users.noreply.github.com>
2025-02-25 16:23:42 +0900

    update

--- create-new-data.js
+++ create-new-data.js
@@ -6,15 +6,15 @@ const body = process.argv[2].split('\n');
 console.log(body);
 const id = process.argv[3];
 console.log(id);
-const name = body[0].match(/name : '(.*)'/);
-const url = body[1].match(/url : '(.*)'/);
+const name = body[0].match(/name : '(.*)'/)[0];
+const url = body[1].match(/url : '(.*)'/)[0];
 // name のバリデーション
 if (name.length > 100) {
     console.error("Error: name must be 100 characters or less.");
     process.exit(1);
 }
-const arr = [name[0], url[0]];
-console.log(arr);
+const arr = [name, url];
+console.log(url);
 // url のバリデーション
 if (!urlPattern.test(url) || url.length > 200) {
     console.error("Error: url must be a valid http(s) URL and 200 characters or less.");
