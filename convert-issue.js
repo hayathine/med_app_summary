07dfe63a6a9610eed6d2121bc707e5379824de95
hayathine <97364703+hayathine@users.noreply.github.com>
2025-02-25 13:53:41 +0900

    update convert-issue

--- convert-issue.js
+++ convert-issue.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log("issueJson:", process.argv); 
-const issueJson = JSON.parse(process.argv);
-const fileName = `issue-${issueJson.number}.json`;
+console.log("issueJson:", process.argv);
+const commentBody = process.argv;
+const unescapedBody = commentBody.replace(/\\\\/g, '\\');
+
+try {
+    const commentData = {
+        body: JSON.parse(unescapedBody),
+    };
+
+} catch (error) {
+    console.error("JSON parse error:", error);
+    // エラー処理
+    process.exit(1);
+}
+console.log("commentData:", commentData);
+
+const commentJson = JSON.stringify(commentData, null, 2);
+const fileName = `comment-${Date.now()}.json`;
 const filePath = path.join('assets', fileName); // assetsフォルダへのパス
 
-fs.writeFileSync(filePath, JSON.stringify(issueJson, null, 2));
+fs.writeFileSync(filePath, commentJson);
